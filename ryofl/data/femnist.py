4d21f8d55d6a7e0a09784a8ec881db28e64a83c4
ClonedOne <severi.giorgio@gmail.com>
2021-04-04 18:03:16 -0400

    added full dataset loading femnist

--- ryofl/data/femnist.py
+++ ryofl/data/femnist.py
@@ -1,8 +1,70 @@
 import os
 
+import h5py
+import numpy as np
 
-def load_client_data(client_id):
+from numpy import ndarray
 
-    # if the preprocessing has not been run throw eception
-    if not os.path.isfile(''):
-        raise FileNotFoundError('Run preprocessing script first')
+from ryofl import common
+
+
+def _load_full():
+    """ Load the entire FEMNIST dataset
+
+    Raises:
+        FileNotFoundError: Requires the preprocessing script to be run
+
+    Returns:
+        (ndarray, ndarray, ndarray, ndarray): trn_x, trn_y, tst_x, tst_y
+    """
+
+    _dir = os.path.join(common.image_data_dir, 'datasets')
+    trn_pth = os.path.join(_dir, 'fed_emnist_digitsonly_train.h5')
+    tst_pth = os.path.join(_dir, 'fed_emnist_digitsonly_test.h5')
+    trn_x_acc = []
+    trn_y_acc = []
+    tst_x_acc = []
+    tst_y_acc = []
+
+    try:
+        trn_file = h5py.File(trn_pth, 'r')
+    except OSError:
+        raise FileNotFoundError(
+            '{} missing, run generate_datasets.py'.format(trn_pth))
+    try:
+        tst_file = h5py.File(tst_pth, 'r')
+    except OSError:
+        raise FileNotFoundError(
+            '{} missing, run generate_datasets.py'.format(tst_pth))
+
+    for client_id in sorted(trn_file['examples']):
+        trn_cli = trn_file['examples'][client_id]
+        tst_cli = tst_file['examples'][client_id]
+
+        # True labels
+        labels_trn = trn_cli['label'][()]
+        labels_tst = tst_cli['label'][()]
+
+        # Insert channels axis
+        matrix_trn = np.expand_dims(trn_cli['pixels'][()], axis=-1)
+        matrix_tst = np.expand_dims(tst_cli['pixels'][()], axis=-1)
+
+        # Accumulate
+        trn_x_acc.append(matrix_trn)
+        trn_y_acc.append(labels_trn)
+        tst_x_acc.append(matrix_tst)
+        tst_y_acc.append(labels_tst)
+
+    trn_x = np.concatenate(trn_x_acc)
+    trn_y = np.concatenate(trn_y_acc)
+    tst_x = np.concatenate(tst_x_acc)
+    tst_y = np.concatenate(tst_y_acc)
+
+    return trn_x, trn_y, tst_x, tst_y
+
+
+def load_data(clients, fraction):
+
+    #  If the clients parameter is None, return the entire training and test sets
+    if clients is None:
+        trn_x, trn_y, tst_x, tst_y = _load_full()
--- ryofl/data/utils_data.py
+++ ryofl/data/utils_data.py
@@ -1,8 +1,18 @@
 import os
 import h5py
 
+from ryofl.data import femnist
 
-def load_dataset(dataset, fraction):
-    pass
 
+def load_dataset(dataset, clients=None, fraction=1.0):
+
+    if dataset == 'femnist':
+        return femnist.load_data(clients, fraction)
+
+    elif dataset == 'cifar100':
+        #  cifar.load_client_data()
+        pass
+
+    else:
+        raise NotImplementedError('Dataset {} not supported'.format(dataset))
 
