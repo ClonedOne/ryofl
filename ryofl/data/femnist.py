64e0fb20e56e81c3e26a1b4482c7f199b51afec3
ClonedOne <severi.giorgio@gmail.com>
2021-04-05 16:18:21 -0400

    updates to data loading

--- ryofl/data/femnist.py
+++ ryofl/data/femnist.py
@@ -4,12 +4,16 @@ import h5py
 import numpy as np
 
 from numpy import ndarray
+from sklearn.model_selection import train_test_split
 
 from ryofl import common
 
 
-def _load_full():
+def _load_full(base_dir=''):
     """ Load the entire FEMNIST dataset
+
+    Args:
+        base_dir (str): overwrite default data dir
 
     Raises:
         FileNotFoundError: Requires the preprocessing script to be run
@@ -18,7 +22,11 @@ def _load_full():
         (ndarray, ndarray, ndarray, ndarray): trn_x, trn_y, tst_x, tst_y
     """
 
-    _dir = os.path.join(common.image_data_dir, 'datasets')
+    if not base_dir:
+        _dir = os.path.join(common.image_data_dir, 'datasets')
+    else:
+        _dir = base_dir
+
     trn_pth = os.path.join(_dir, 'fed_emnist_digitsonly_train.h5')
     tst_pth = os.path.join(_dir, 'fed_emnist_digitsonly_test.h5')
     trn_x_acc = []
@@ -63,8 +71,68 @@ def _load_full():
     return trn_x, trn_y, tst_x, tst_y
 
 
-def load_data(clients, fraction):
+def load_single_client(client_id, base_dir=''):
+    """ Load the FEMNIST data for a single client
 
-    #  If the clients parameter is None, return the entire training and test sets
+    Args:
+        client_id (str): id of the client's data to load
+        base_dir (str): overwrite default data dir
+
+    Returns:
+        (ndarray, ndarray, ndarray, ndarray): trn_x, trn_y, tst_x, tst_y
+    """
+
+
+    if not base_dir:
+        _dir = common.femnist_clients_dir
+    else:
+        _dir = base_dir
+
+    client_f = os.path.join(_dir, '{}_cli-' + str(client_id) + '_{}')
+
+    trn_x = np.load(client_f.format('trn', 'x'), allow_pickle=True)
+    trn_y = np.load(client_f.format('trn', 'y'), allow_pickle=True)
+    tst_x = np.load(client_f.format('tst', 'x'), allow_pickle=True)
+    tst_y = np.load(client_f.format('tst', 'y'), allow_pickle=True)
+
+    return trn_x, trn_y, tst_x, tst_y
+
+
+
+def load_data(clients, frac=1.0):
+    """ Load data wrapper for FEMNIST
+
+    Args:
+        clients: (list, ndarray, string) ids of the clients to load
+        frac: fraction of the data to sample
+
+    Returns:
+        (ndarray, ndarray, ndarray, ndarray): trn_x, trn_y, tst_x, tst_y
+    """
+
+    #  If the clients parameter is None, return the entire training and test
+    #  sets
     if clients is None:
         trn_x, trn_y, tst_x, tst_y = _load_full()
+
+    # If multiple clients are passed, and the number of clients is > number of
+    # processes define in the config, use multiprocessing to load the data
+    #  elif isinstance(clients, (ndarray, list)):
+    #      pass
+
+    # Single client id has been passed
+    elif isinstance(clients, str):
+        trn_x, trn_y, tst_x, tst_y = load_single_client(client_id=clients)
+
+    else:
+        raise NotImplementedError('clients: {} not supported'.format(clients))
+
+    if frac != 1.0:
+        print('Sampling factor: ', frac)
+        _, trn_x = train_test_split(trn_x, test_size=frac, random_state=0, stratify=trn_y)
+        _, trn_y = train_test_split(trn_y, test_size=frac, random_state=0, stratify=trn_y)
+        _, tst_x = train_test_split(tst_x, test_size=frac, random_state=0, stratify=tst_y)
+        _, tst_y = train_test_split(tst_y, test_size=frac, random_state=0, stratify=tst_y)
+
+    return trn_x, trn_y, tst_x, tst_y
+
--- ryofl/data/utils_data.py
+++ ryofl/data/utils_data.py
@@ -5,6 +5,21 @@ from ryofl.data import femnist
 
 
 def load_dataset(dataset, clients=None, fraction=1.0):
+    """ Wrapper function to load data from a dataset
+
+    Can load a single client, multiple clients, or the entire dataset.
+
+    Args:
+        dataset (str): identifier of the dataset to load
+        clients: (string, list, ndarray) ids of the clients to load
+        fraction (float): fraction of the data to sample
+
+    Raises:
+        NotImplementedError: dataset must belong to list of supported datasets
+
+    Returns:
+        (ndarray, ndarray, ndarray, ndarray): trn_x, trn_y, tst_x, tst_y
+    """
 
     if dataset == 'femnist':
         return femnist.load_data(clients, fraction)
